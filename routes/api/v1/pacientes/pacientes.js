95a9954bb7508f9696678d636682c381ced69a57
Orlando J Betancourth <orlando.betancourth@gmail.com>
2022-01-20 18:40:48 -0600

    20220120

--- routes/api/v1/pacientes/pacientes.js
+++ routes/api/v1/pacientes/pacientes.js
@@ -8,7 +8,26 @@ router.get('/', (req, res) => {
       updates: new Date(2022,0,19,18,41,00)
     }
   );
-});
+}); //GET /
+
+router.post('/new', async (req, res) => {
+  const { nombres, apellidos, identidad, email, telefono } = req.body;
+
+  res.status(200).json(
+    {
+      status: 'ok',
+      recieved: {
+        nombres,
+        apellidos,
+        nombreCompleto: `${nombres} ${apellidos}`,
+        identidad,
+        email,
+        telefono
+      }
+    });
+}); //POST /new
+
+
 //router.post();
 //router.put();
 //router.delete();
