7f850500d8c0918e2e0712184333bb660b8b96a3
Orlando J Betancourth <orlando.betancourth@gmail.com>
2022-03-15 18:58:47 -0600

    20220315 cors

--- app.js
+++ app.js
@@ -2,6 +2,19 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors');
+var whiteList = (process.env.CORS_ORIGIN || 'http://localhost:3001').split(',');
+console.log('WhiteList', whiteList);
+var corsOptions = {
+  origin: (origin, callback)=>{
+    console.log('Origin Value:', origin);
+    if (whiteList.indexOf(origin) >= 0){
+      callback(null, true);
+    } else {
+      callback(new Error('CORS not allowed'));
+    }
+  }
+}
 
 var indexRouter = require('./routes/index');
 var apiRouter = require('./routes/api/api');
@@ -9,6 +22,7 @@ var apiRouter = require('./routes/api/api');
 var app = express();
 
 app.use(logger('dev'));
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
